2003fb75934a199289ada5716a72f401691a8e22
phobos <phobos@local>
2025-09-25 22:35:11 -0400

    fixed topic monitor on jackals

--- system-status/src/system_status/jackal_monitor.py
+++ system-status/src/system_status/jackal_monitor.py
@@ -9,19 +9,38 @@ import rospy
 from typing import Dict
 from rospy.timer import TimerEvent
 from dtc_msgs.msg import JackalStatus
-
+from sensor_msgs.msg import NavSatFix, Imu, Image, Joy
+from audio_common_msgs.msg import AudioData
 
 class JackalMonitor:
 
     def __init__(self) -> None:
         self.topics_ = rospy.get_param("/jackal_status/jackal/topics")
         self.rtk_ = False
-        self.timer_ = rospy.Timer(rospy.Duration(2.0), self.statusCallback)
-        rospy.Subscriber("/ublox/fix", NavSatFix, self.rtkCallback)
+        self.gps_ = False
+        self.rgb_ = False
+        self.joy_ = False
+        self.ir_ = False
+        self.ouster_ = False
 
+        self.gps_time_ = rospy.Time.now()
+        self.mic_time_ = rospy.Time.now()
+        self.joy_time_ = rospy.Time.now()
+        self.cam_time_ = rospy.Time.now()
+        self.ir_time_ = rospy.Time.now()
+        self.ouster_time_ = rospy.Time.now()
+
+        rospy.Subscriber("/ublox/fix", NavSatFix, self.rtkCallback)
+        rospy.Subscriber("/boson/image_raw", Image, self.bosonCallback)
+        rospy.Subscriber("/camera/image_color", Image, self.cameraCallback)
+        rospy.Subscriber("/joy", Joy, self.joyCallback)
+        rospy.Subscriber("/audio/audio", AudioData, self.audioCallback)
+        rospy.Subscriber("/ouster/imu", Imu, self.ousterCallback)
+        
+        self.timer_ = rospy.Timer(rospy.Duration(2.0), self.statusCallback)
         print("[JACKAL-STATUS] Using topics: ", self.topics_)
             
-        self.pub_ = rospy.Publisher("/status", JackalStatus, queue_size=1)
+        self.pub_ = rospy.Publisher("/sensor_status", JackalStatus, queue_size=1)
         print("[JACKAL-STATUS] Jackal Status Monitor Initialized")
 
 
@@ -29,19 +48,31 @@ class JackalMonitor:
         for key, val in dictionary.items():
             if val == value:
                 return key
-            return None
+        return None
 
     def rtkCallback(self, msg : NavSatFix) -> None:
-        if msg.position_covarinace[0] < 1.0 and msg.position_covariance[1] < 1.0:
+        if msg.position_covarinace[0] < 0.2 and msg.position_covariance[1] < 0.2:
             self.rtk_ = True
         else:
             self.rtk_ = False
+        self.gps_time_ = rospy.Time.now()
+    
+    def bosonCallback(self, msg : Image) -> None:
+        self.ir_time_ = rospy.Time.now()
+
+    def cameraCallback(self, msg : Image) -> None:
+        self.cam_time_ = rospy.Time.now()
+
+    def joyCallback(self, msg : Joy) -> None:
+        self.joy_time_ = rospy.Time.now()
+
+    def audioCallback(self, msg : AudioData) -> None:
+        self.mic_time_ = rospy.Time.now()
+
+    def ousterCallback(self, msg : Imu) -> None:
+        self.ouster_time_ = rospy.Time.now()
 
     def statusCallback(self, event : TimerEvent) -> None:
-        active_topics = rospy.get_published_topics()
-        
-        active_topic_list = [topic[0] for topic in active_topics]
-
         msg = JackalStatus()
 
         msg.rgb = False
@@ -52,25 +83,21 @@ class JackalMonitor:
         msg.rtk = False
         msg.mic = False
 
-        for topic in self.topics_:
-            if topic in self.topics_:
-                key = self.find_key_by_value(self.topics_, topic)
-                if key == "rgb":
-                    msg.rgb = True
-                elif key == "thermal":
-                    msg.thermal = True
-                elif key == "gps":
-                    msg.gps = True
-                elif key == "mic":
-                    msg.mic = True
-                elif key == "controller":
-                    msg.jeti = True
-                elif key == "ouster":
-                    msg.ouster = True
+        if rospy.Time.now() - self.gps_time_ < rospy.Duration(10.0):
+            msg.gps = True
+            msg.rtk = self.rtk_
+        if rospy.Time.now() - self.ir_time_ < rospy.Duration(10.0):
+            msg.thermal = True 
+        if rospy.Time.now() - self.cam_time_ < rospy.Duration(10.0):
+            msg.rgb = True
+        if rospy.Time.now() - self.joy_time_ < rospy.Duration(10.0):
+            msg.jeti = True
+        if rospy.Time.now() - self.mic_time_ < rospy.Duration(10.0):
+            msg.mic = True 
+        if rospy.Time.now() - self.ouster_time_ < rospy.Duration(10.0):
+            msg.ouster = True
 
-        msg.rtk = self.rtk_
-
-        msg.header.stamp = rospy.Timer.now()
+        msg.header.stamp = rospy.Time.now()
         msg.header.frame_id = "jackal"
 
         self.pub_.publish(msg)
