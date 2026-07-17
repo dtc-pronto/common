d2bcab18b89f237ab1b4e5ebd18e16e673d3250d
Lee Jenshil <leejenshil@gmail.com>
2026-07-17 14:54:37 -0400

    Fix run.bash to make .env file optional

--- run.bash
+++ run.bash
@@ -2,13 +2,18 @@
 
 xhost +
 
+ENV_ARGS=""
+if [ -f ~/basestation/.env ]; then
+  ENV_ARGS="--env-file ~/basestation/.env"
+fi
+
 docker run --rm -it \
   --network=host \
   --privileged \
   --hostname `hostname` \
   -v "/tmp/.X11-unix:/tmp/.X11-unix" \
   -v "/dev:/dev" \
-  --env-file ~/basestation/.env \
+  $ENV_ARGS \
   -e DISPLAY=$DISPLAY \
   -e QT_X11_NO_MITSHM=1 \
   -e XAUTHORITY=$XAUTH \
